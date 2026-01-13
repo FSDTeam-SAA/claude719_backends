897fdc19d304a401f99f3b35ab7c9d7cfcba489e
Wrong1234 <mahabur24cse@gmail.com>
2026-01-13 14:08:30 +0600

    update

--- src/app/modules/dashboard/dashboard.routes.ts
+++ src/app/modules/dashboard/dashboard.routes.ts
@@ -7,11 +7,10 @@ const router = Routes();
 
 router.get('/overview', auth(userRole.admin), dashboardController.dashboardOverview)
 router.get('/monthly-revenue-chart', auth(userRole.admin), dashboardController.getMonthlyReveneueChart);
-router.get('/all-player-reveneue', auth(userRole.admin), dashboardController.getAllPlayersRevenue);
-router.get('/all-team-reveneue', auth(userRole.admin), dashboardController.getAllTeamReveneue);
 router.get('/single-player-view/:id', auth(userRole.admin), dashboardController.singleplayerView);
 router.get('/single-team-view/:id', auth(userRole.admin), dashboardController.singleTeamView);
 router.delete('/delete-player-account/:id', auth(userRole.admin), dashboardController.deletePlayerAccount);
 router.delete('/delete-team-account/:id', auth(userRole.admin), dashboardController.deleteTeamAccount);
+router.get('/total-revenue', auth(userRole.admin), dashboardController.totalRevenue);
 
 export const dashboardRouter = router;
