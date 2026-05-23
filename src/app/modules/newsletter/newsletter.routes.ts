d7df14515f72b8a250fcbc5d6052fe541a3173cf
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-05-23 10:07:56 +0600

    conflict solve

--- src/app/modules/newsletter/newsletter.routes.ts
+++ src/app/modules/newsletter/newsletter.routes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import { newsletterController } from './newsletter.controller';
 
@@ -11,14 +10,3 @@ router.delete('/:id', newsletterController.deleteNewsletter);
 router.post('/broadcast', newsletterController.broadcastNewsletter);
 
 export const newsletterRouter = router;
-=======
-import express from 'express';
-import { newsletterController } from './newsletter.controller';
-
-const router = express.Router();
-
-router.post('/', newsletterController.createNewsLetter);
-router.post('/broadcast', newsletterController.broadcastNewsletter);
-
-export const newsletterRouter = router;
->>>>>>> db721c6cb623b5dd4f7c12d147712521536566dc
